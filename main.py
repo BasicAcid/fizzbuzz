4bc2af509b20a73615002f01291a318aa1e24419
David Tabarie <david.tabarie@gmail.com>
2018-12-03 17:53:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+def fizzbuzz(x):
+    if (x%3 == 0):
+        print("Fizz")
+    elif (x%5 == 0):
+        print("Buzz")
+    else:
+        print(n)
+
+for n in range(1, 100):
+    fizzbuzz(n)
